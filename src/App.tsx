2c116d5eec07019765b27f661a2328b545b190c9
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-10-06 18:06:18 -0300

    connect api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+
+interface Advice {
+  slip: {
+    id: string
+    advice: string
+  }
+}
 
 export const App = () => {
-  return <></>
+  // eslint-disable-next-line no-empty-pattern
+  const [data, setData] = useState<Advice>({
+    slip: {
+      id: '', // setando estado inicial com string vazia
+      advice: '', // setando estado inicial com string vazia
+    },
+  })
+
+  const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    setData(data)
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  return (
+    <>
+      <h1>{data.slip.id}</h1>
+      <h2>{data.slip.advice}</h2>
+      <button onClick={fetchAdvice}>Get Advice</button>
+    </>
+  )
 }
 
 export default App
